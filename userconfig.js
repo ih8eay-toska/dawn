ad787f13e9d4aeec1c643726af47b05d95c1c34c
Nguyễn Khánh Phong <63902436+ih8eay-toska@users.noreply.github.com>
2024-12-20 05:30:06 +0700

    zzz

--- userconfig.js
+++ userconfig.js
@@ -36,22 +36,10 @@ const CONFIG = new Config({
         name: 'fun',
         links: [
           {
-            url: 'https://www.youtube.com/',
-            name: 'youtube',
-            icon: 'brand-youtube',
-            icon_color: '#8B6464'
-          },
-          {
-            name: 'steam',
-            url: 'https://steamcommunity.com/',
-            icon: 'brand-steam',
-            icon_color: '#647B8B'
-          },
-          {
-            name: 'reddit',
-            url: 'https://www.reddit.com/',
-            icon_color: '#8B7564',
-            icon: 'brand-reddit',
+            name: 'facebook',
+            url: 'https://www.facebook.com/',
+            icon: 'brand-facebook',
+            icon_color: '#648B7D'
           },
           {
             name: 'twitter',
@@ -60,10 +48,51 @@ const CONFIG = new Config({
             icon_color: '#648B7D'
           },
           {
-            name: 'facebook',
-            url: 'https://www.facebook.com/',
-            icon: 'brand-facebook',
-            icon_color: '#648B7D'
+            name: 'reddit',
+            url: 'https://www.reddit.com/',
+            icon_color: '#8B7564',
+            icon: 'brand-reddit',
+          },
+          {
+            name: 'steam',
+            url: 'https://steamcommunity.com/',
+            icon: 'brand-steam',
+            icon_color: '#647B8B'
+          },
+          {
+            name: 'weeb-central',
+            url: 'https://weebcentral.com/',
+            icon: 'books',
+            icon_color: '#647B8B'
+          }
+        ]
+      },
+      {
+        name: 'music services',
+        links: [
+          {
+            url: 'https://soundcloud.com/',
+            icon: 'brand-soundcloud',
+            icon_color: '#c57750'
+          },
+          {
+            url: 'https://open.spotify.com/',
+            icon: 'brand-spotify',
+            icon_color: '#b2fba5'
+          },
+          {
+            url: 'https://r-a-d.io/',
+            icon: 'radio'
+          },
+          {
+            url: 'https://www.youtube.com/',
+            icon: 'brand-youtube',
+            icon_color: '#8B6464'
+          },
+          {
+            url: 'https://mynoise.net/',
+            name: 'background noise',
+            icon: 'disc'
           }
         ]
       },
@@ -82,7 +111,7 @@ const CONFIG = new Config({
           },
           {
             name: 'mail',
-            url: 'https://mail.google.com/mail/u/1/#inbox',
+            url: 'https://mail.google.com/mail/u/3/#inbox',
             icon: 'mail'
           },
           {
@@ -104,30 +133,6 @@ const CONFIG = new Config({
       background_url: 'src/img/banners/bg-2.gif',
       categories: [
         {
-          name: 'music services',
-          links: [
-            {
-              url: 'https://soundcloud.com/',
-              icon: 'brand-soundcloud',
-              icon_color: '#c57750'
-            },
-            {
-              url: 'https://youtu.be/',
-              icon: 'brand-youtube',
-              icon_color: '#996767'
-            },
-            {
-              url: 'https://open.spotify.com/',
-              icon: 'brand-spotify',
-              icon_color: '#b2fba5'
-            },
-            {
-              url: 'https://r-a-d.io/',
-              icon: 'radio'
-            },
-          ]
-        },
-        {
           name: 'Twitch',
           links: [
             {
@@ -145,16 +150,42 @@ const CONFIG = new Config({
           ]
         },
         {
-          name: 'music boards',
+          name: 'xxx',
           links: [
             {
-              name: '/music/',
-              url: 'https://lainchan.org/music/',
-              icon: 'disc'
+              name: 'waifu2x',
+              url: 'https://waifu2x.booru.pics/',
+              icon: 'photo-scan'
             },
             {
-              name: '/mu/',
-              url: 'https://boards.4channel.org/mu/',
+              name: 'hitomi',
+              url: 'https://hitomi.la/',
+              icon: 'eye-exclamation'
+            }
+          ]
+        },
+        {
+          name: 'boards',
+          links: [
+            {
+              name: '/r9k/',
+              url: 'https://boards.4chan.org/r9k/',
+              icon: 'robot-off'
+            },
+            {
+              name: '/w/',
+              url: 'https://boards.4chan.org/w/',
+              icon: 'background'
+            },
+            {
+              name: '/wg/',
+              url: 'https://boards.4chan.org/wg/',
+              icon: 'box-multiple'
+            },
+            {
+              name: '/w/',
+              url: 'https://boards.4chan.org/wsg/',
+              icon: 'gif'
             }
           ]
         }
@@ -165,59 +196,23 @@ const CONFIG = new Config({
       background_url: 'src/img/banners/bg-3.gif',
       categories: [
         {
-          name: 'subreddits',
+          name: 'modding',
           links: [
             {
-              name: 'r/startpages/',
-              url: 'https://www.reddit.com/r/startpages/'
+              name: 'modrinth',
+              url: 'https://modrinth.com/',
+              icon: 'diamond'
             },
             {
-              name: 'r/unixporn',
-              url: 'https://www.reddit.com/r/unixporn/'
+              name: 'nexus',
+              url: 'https://www.nexusmods.com/',
+              icon: 'box-padding'
             },
             {
-              name: 'r/mechkbds/',
-              url: 'https://www.reddit.com/r/MechanicalKeyboards/',
-              icon: 'keyboard',
+              name: 'gamebanana',
+              url: 'https://gamebanana.com/',
+              icon: 'device-nintendo',
               icon_color: '#a57685'
-            },
-            {
-              name: 'r/programming',
-              url: 'https://www.reddit.com/r/programming/'
-            }
-          ]
-        },
-        {
-          name: 'blogs',
-          links: [
-            {
-              name: 'fasterthanli',
-              url: 'https://fasterthanli.me/articles',
-              icon: 'anchor'
-            },
-            {
-              name: 'dev.to',
-              url: 'https://dev.to'
-            },
-            {
-              name: 'mataroa.blog',
-              url: 'https://collection.mataroa.blog',
-            }
-          ]
-        },
-        {
-          name: 'misc',
-          links: [
-            {
-              name: 'post office',
-              url: 'http://afternoon.dynu.com/letterbox.html',
-              icon: 'mailbox'
-            },
-            {
-              name: 'rust docs',
-              url: 'https://doc.rust-lang.org/book/',
-              icon: 'notebook',
-              icon_color: '#977a3a'
             }
           ]
         }
